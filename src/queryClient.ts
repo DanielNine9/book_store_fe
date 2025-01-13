72082936208cb6231200238c0332a44ead4d324d
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-13 16:56:20 +0700

    Create queryClient.ts

---
+++ src/queryClient.ts
@@ -0,0 +1,15 @@
+// src/queryClient.ts
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, 
+    //   cacheTime: 1000 * 60 * 10,
+      retry: 2, 
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
+
+export default queryClient;

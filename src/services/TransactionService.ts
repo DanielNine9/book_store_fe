50dffdcb1ded21a555a545ff7c437f470e5f9140
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-13 16:56:17 +0700

    Create TransactionService.ts

---
+++ src/services/TransactionService.ts
@@ -0,0 +1,18 @@
+import axiosInstance from "../utils/axiosConfig";
+
+export const fetchAllTransaction = async (page: number, limit: number, search: string, searchFields: string, searchOperator: string) => {
+    try {
+      const response = await axiosInstance.get('/admin/transactions', {
+        params: {
+          page,
+          limit,
+          search,
+          search_fields: searchFields,
+          search_operator: searchOperator,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };

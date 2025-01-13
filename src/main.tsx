c6d2ac6c6ef63929ccbe6400fcb7b0bf31575142
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-13 16:56:59 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-const queryClient = new QueryClient();
+import queryClient from "./queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>

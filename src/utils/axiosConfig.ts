d552ad77949d2c7ccfe1335da4cc9f6d57e8d2d8
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-15 15:40:42 +0700

    Update axiosConfig.ts

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,13 +1,30 @@
 import axios from 'axios';
+
 // Cấu hình Axios với base URL và các thiết lập mặc định
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
-  timeout: 10000, 
+  baseURL: import.meta.env.VITE_API_URL,  
+  timeout: 10000,  // Timeout sau 10 giây
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
+    'Content-Type': 'application/json',  // Đảm bảo gửi dữ liệu dưới dạng JSON
+    'Accept': 'application/json',        // Accept JSON trả về
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const token = user?.token;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 axiosInstance.interceptors.response.use(
   (response) => response, 
